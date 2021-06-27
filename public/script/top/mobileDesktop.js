e50c84ea1b92ef5f86268e83227d82e765be61a9
Ihsan Rakhman <66576783+neutrixs@users.noreply.github.com>
2021-06-27 15:02:20 +0700

    temp

---
+++ public/script/top/mobileDesktop.js
@@ -0,0 +1,22 @@
+const mobile = function(){
+    $('#holder').addClass('margin0')
+}
+const desktop = function(){
+    $('#holder').removeClass('margin0')
+}
+
+let changeAt = 44.5
+if($(window).width() / parseFloat($("body").css("font-size")) < changeAt) mobile();
+$('#holder').removeClass('none').addClass('ts300ms')
+
+window.onresize = function(){
+    width = this.innerWidth;
+    
+    emWidth = $(window).width() / parseFloat($("body").css("font-size"))
+    if(emWidth < changeAt){
+        mobile()
+    }
+    else{
+        desktop()
+    }
+}

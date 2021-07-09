0e8c0f7dcdaffe533b47629e3db212f2846b36ad
Ihsan Rakhman <66576783+neutrixs@users.noreply.github.com>
2021-07-09 19:18:26 +0700

    simplify

--- public/script/colour.js
+++ public/script/colour.js
@@ -1,57 +1,56 @@
-paletteDark = ''
-paletteLight = ''
+palette = ''
 const colour = new ColorThief()
 let img = document.getElementById('mainPicture')
 img.crossOrigin = 'Anonymous'
 img.addEventListener('load',function(){
-    paletteDark = (colour.getColor(img)).join(',')
-    paletteLight = paletteDark
+    let currentColour = (colour.getColor(img)).join(',')
+    palette = [currentColour,currentColour]
 
-    checkDarkPalette(paletteDark)
-    checkLightPalette(paletteLight)
+    checkDarkPalette(palette[0])
+    checkLightPalette(palette[1])
 
     setPalette()
 })
 
 const setPalette = function(){
-    $('#nowPlaying').css('background-color',`rgb(${window.localStorage['dark']=='false'?paletteLight:paletteDark})`)
+    $('#nowPlaying').css('background-color',`rgb(${window.localStorage['dark']=='false'?palette[1]:palette[0]})`)
 }
 
-const checkDarkPalette = function(paletteDarkk){
-    paletteDarkk = paletteDarkk.split(',')
-    let paletteDark2 = [...paletteDarkk]
+const checkDarkPalette = function(paletteDark){
+    paletteDark = paletteDark.split(',')
+    let paletteDark2 = [...paletteDark]
 
-    if(paletteDarkk.sort((a,b)=>{return a-b})[2] > 128){
-        let divideBy = paletteDarkk[2] / 128
+    if(paletteDark.sort((a,b)=>{return a-b})[2] > 128){
+        let divideBy = paletteDark[2] / 128
 
         for(i=0;i<paletteDark2.length;i++){
             paletteDark2[i]= Math.floor(paletteDark2[i]/divideBy)
         }
 
-        paletteDark = paletteDark2.join(',')
+        palette[0] = paletteDark2.join(',')
     }
-    else if(paletteDarkk.sort((a,b)=>{return a-b})[2] < 32){
-        let divideBy = 32 / paletteDarkk[2]
+    else if(paletteDark.sort((a,b)=>{return a-b})[2] < 32){
+        let divideBy = 32 / paletteDark[2]
 
         for(i=0;i<paletteDark2.length;i++){
             paletteDark2[i] = Math.floor(paletteDark2[i]*divideBy)
         }
 
-        paletteDark = paletteDark2.join(',')
+        palette[0] = paletteDark2.join(',')
     }
 }
 
-const checkLightPalette = function(paletteLightt){
-    paletteLightt = paletteLightt.split(',')
-    let paletteLight2 = [...paletteLightt]
+const checkLightPalette = function(paletteLight){
+    paletteLight = paletteLight.split(',')
+    let paletteLight2 = [...paletteLight]
 
-    if(paletteLightt.sort((a,b)=>{return a-b})[2] < 255){
-        let divideBy = 255 / paletteLightt[2]
+    if(paletteLight.sort((a,b)=>{return a-b})[2] < 255){
+        let divideBy = 255 / paletteLight[2]
 
         for(i=0;i<paletteLight2.length;i++){
             paletteLight2[i]= Math.floor(paletteLight2[i]*divideBy)
         }
 
-        paletteLight = paletteLight2.join(',')
+        palette[1] = paletteLight2.join(',')
     }
 }

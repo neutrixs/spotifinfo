b2f43e0ba9922910a9e52fc913d0d233a206ac7e
Ihsan Rakhman <66576783+neutrixs@users.noreply.github.com>
2021-07-10 16:51:34 +0700

    Update uglify.js

--- minifier/uglify.js
+++ minifier/uglify.js
@@ -6,7 +6,7 @@ for(const currentDir of dir){
     let listFile = fs.readdirSync(currentDir).filter(f=>f.endsWith('.js')).filter(f=>f!=='jquery.js' && f!=='he.js' && !f.includes('minify'))
     for(let file of listFile){
         let content = fs.readFileSync(currentDir+'/'+file,{encoding:'utf-8'})
-        let res = uglify(content)
+        let res = uglify.minify(content)
 
         fs.writeFileSync(currentDir.replace('/script','/script_minify')+'/'+file,res.code)
     }

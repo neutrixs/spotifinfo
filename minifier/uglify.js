78a63576a2c5306c8b4e449dbef0a91f102441b3
Ihsan Rakhman <ihsan.maulana0501@gmail.com>
2021-08-03 19:04:58 +0700

    merged from ultra-minify

--- minifier/uglify.js
+++ minifier/uglify.js
@@ -1,24 +1,15 @@
 const fs = require('fs')
 const uglify = require('uglify-js')
-const dir = ['./public/script','./public/script/top','./public/script/acc']
-const mangleEval = ['./public/script/nowPlaying.js','./public/script/recentlyPlayed.js','./public/script/top/getTop.js']
-let shouldMangleEval,mangleOption;
+const listUglify = JSON.parse(fs.readFileSync('./minifier/list_uglify.json'))
 
-for(const currentDir of dir){
-    let listFile = fs.readdirSync(currentDir).filter(f=>f.endsWith('.js')).filter(f=>f!=='jquery.js' && f!=='he.js' && !f.includes('minify'))
-    for(let file of listFile){
-        let content = fs.readFileSync(currentDir+'/'+file,{encoding:'utf-8'})
-        if(mangleEval.includes(currentDir+'/'+file)){
-            shouldMangleEval = true
-        }
-        else{
-            shouldMangleEval = false
-        }
+for(let bundle in listUglify){
+    let fileData = {}
+    for(file of listUglify[bundle]){
+        let fileName = file.split('/')
+        fileName = fileName[fileName.length-1]
 
-        mangleOption = shouldMangleEval?{eval:true}:true
-
-        let res = uglify.minify(content,{mangle:mangleOption})
-
-        fs.writeFileSync(currentDir.replace('/script','/script_minify')+'/'+file,res.code)
+        fileData[fileName] = fs.readFileSync(file,{encoding:'utf-8'})
     }
+    let res = (uglify.minify(fileData,{mangle:{eval:true,toplevel:true}})).code
+    fs.writeFileSync('./public/script_minify/'+bundle+'.js',res)
 }

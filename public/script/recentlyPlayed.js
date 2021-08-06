f9610e16997dc38e6ce081dd01f6f7e1d531fea8
Ihsan Rakhman <66576783+neutrixs@users.noreply.github.com>
2021-08-06 22:26:22 +0700

    No eval and IIFE bit (#8)
    * changes
    
    * minified
    
    * better for minifier
    
    * one done and fuck this shit
    
    * ok i make it local
    
    * changes
    
    * tracks done
    
    * half but pls work haha
    
    * work fuck yeah
    
    * i forgor 💀
    
    * with IIFE
    
    * what is this

--- public/script/recentlyPlayed.js
+++ public/script/recentlyPlayed.js
@@ -1,4 +1,10 @@
 const getRecentlyPlayed = async function(){
+    let createElement = 'createElement' // best for minifier, but doesn't make it hard to code either
+    let setAttribute = 'setAttribute'
+    let items = 'items'
+    let track = 'track'
+    let appendChild = 'appendChild'    
+
     let res = await fetch('https://api.spotify.com/v1/me/player/recently-played?limit=50',{
         method:'GET',
         headers:{
@@ -15,25 +21,84 @@ const getRecentlyPlayed = async function(){
         return
     }
 
-    let temp = ''
-    for(i=0;i<res.items.length;i++){
-        temp+=`<div id="RecentlyPlayed${i}" class="listRecentlyPlayed"><div id="recentlyPlayed${i}ImageHolder" class="recentlyPlayedImageHolder"><img class="recentlyPlayedImage" src="${res.items[i].track.album.images[1].url}"></div><div id="recentlyPlayed${i}InfoHolder" class="recentlyPlayedInfoHolder"><p class="recentlyPlayedSongName">${res.items[i].track.name}</p><p class="recentlyPlayedArtistName">`
-        for(j=0;j<res.items[i].track.artists.length;j++){
-            temp+=`<span id="recent${i}artist${j}">${res.items[i].track.artists[j].name}</span>`
+    $('#recentlyPlayedListHolder').html('')
 
-            if(j !== res.items[i].track.artists.length-1){
-                temp+=', '
+    for(i=0;i<res[items].length;i++){
+        let recentlyPlayedEach = document[createElement]('div')
+        
+        recentlyPlayedEach[setAttribute]('id','RecentlyPlayed'+i)
+        recentlyPlayedEach[setAttribute]('class','listRecentlyPlayed')
+
+        let recentlyPlayedImageHolder = document[createElement]('div')
+            
+        recentlyPlayedImageHolder[setAttribute]('id','recentlyPlayed'+i+'ImageHolder')
+        recentlyPlayedImageHolder[setAttribute]('class','recentlyPlayedImageHolder')
+
+        let recentlyPlayedImage = document[createElement]('img')
+            
+        recentlyPlayedImage[setAttribute]('class','recentlyPlayedImage')
+        recentlyPlayedImage[setAttribute]('src',res[items][i][track].album.images[1].url)
+
+        let recentlyPlayedInfoHolder = document[createElement]('div')
+        
+        recentlyPlayedInfoHolder[setAttribute]('id','recentlyPlayed'+i+'InfoHolder')
+        recentlyPlayedInfoHolder[setAttribute]('class','recentlyPlayedInfoHolder')
+
+        let recentlyPlayedSongName = document[createElement]('p')
+        
+        recentlyPlayedSongName[setAttribute]('class','recentlyPlayedSongName')
+        recentlyPlayedSongName.innerHTML = res[items][i][track].name
+        
+        let recentlyPlayedArtistName = document[createElement]('p')
+            
+        recentlyPlayedArtistName[setAttribute]('class','recentlyPlayedArtistName')
+        
+        for(j=0;j<res[items][i][track].artists.length;j++){
+            let each = document[createElement]('span')
+                
+            each[setAttribute]('id','recent'+i+'artist'+j)
+            each.innerHTML = res[items][i][track].artists[j].name
+
+            recentlyPlayedArtistName[appendChild](each)
+
+            if(j!= res[items][i][track].artists.length-1){
+                let each = document[createElement]('span')
+                each.innerHTML = ', '
+
+                recentlyPlayedArtistName[appendChild](each)
             }
         }
-        temp+='</p></div></div>'
-    }
-    $('#recentlyPlayedListHolder').html(temp)
 
-    temp = ''
-    for(i=0;i<res.items.length;i++){
-        temp+=`$('#recentlyPlayed${i}InfoHolder').off().on('click',()=>{url = '${res.items[i].track.external_urls.spotify}';if(isMobile()){location.href = url}else{window.open(url)}});$('#recentlyPlayed${i}ImageHolder').off().on('click',()=>{url = '${res.items[i].track.album.external_urls.spotify}';if(isMobile()){location.href = url}else{window.open(url)}})\n`
+        recentlyPlayedInfoHolder[appendChild](recentlyPlayedSongName)
+        recentlyPlayedInfoHolder[appendChild](recentlyPlayedArtistName)
+        recentlyPlayedImageHolder[appendChild](recentlyPlayedImage)
+        recentlyPlayedEach[appendChild](recentlyPlayedImageHolder)
+        recentlyPlayedEach[appendChild](recentlyPlayedInfoHolder)
+
+        let listHolder = document.getElementById('recentlyPlayedListHolder')
+        listHolder[appendChild](recentlyPlayedEach)
     }
-    eval(temp)
+
+    for(i=0;i<res[items].length;i++){
+        $(`#recentlyPlayed${i}InfoHolder`).off().on('click',{res:{...res},i:i},(event)=>{
+            let url = event.data.res.items[event.data.i].track.external_urls.spotify
+            if(isMobile()){
+                location.href = url
+            }
+            else{
+                window.open(url)
+            }
+        })
+        $(`#recentlyPlayed${i}ImageHolder`).off().on('click',{res:{...res},i:i},(event)=>{
+            let url = event.data.res.items[event.data.i].track.album.external_urls.spotify
+            if(isMobile()){
+                location.href = url
+            }
+            else{
+                window.open(url)
+            }
+        })
+    }
 
     $('#recentlyPlayed').removeClass('none')
 }
--- public/script/top/getTop.js
+++ public/script/top/getTop.js
@@ -1,86 +1,203 @@
-const getTopTracks = async function(type){
-    if(type>2) return
+(async function(document){    
+    const getTopTracks = async function(type){
+        let createElement = 'createElement' // best for minifier, but doesn't make it hard to code either
+        let setAttribute = 'setAttribute'
+        let items = 'items'
+        let appendChild = 'appendChild'   
+        if(type>2) return
 
-    let res,term,temp
-    term = ['long_term','medium_term','short_term']
-    res = await fetch('https://api.spotify.com/v1/me/top/tracks?limit=50&time_range='+term[type],{
-        method:'GET',
-        headers:{
-            'Authorization':window.localStorage['token']
-        }
-    })
-    res = await res.json()
-
-    if(res.error){
-        if(res.error.status == 401 || res.error.status == 400){
-            await getToken()
-            await getTopTracks(type);
-        }
-        return
-    }
-
-    temp = ''
-
-    for(i=0;i<res.items.length;i++){
-        temp+=`<div id="listTrack${type}N${i}" class="list"><div class="listNumber">${i+1}</div><div id="listTrack${type}N${i}AlbumHolder" class="listTrackAlbumHolder"><img id="listTrack${type}N${i}Album" class="listTrackAlbum" src="${res.items[i].album.images[1].url}"></div><div id="listTrack${type}N${i}InfoHolder" class="listTrackInfoHolder"><div class="listTrackTitle">${res.items[i].name}</div><div class="listTrackArtist">`
-        
-        for(j=0;j<res.items[i].artists.length;j++){
-            temp+=res.items[i].artists[j].name
-
-            if(j!==res.items[i].artists.length-1){
-                temp+=', '
+        let res,term,temp
+        term = ['long_term','medium_term','short_term']
+        res = await fetch('https://api.spotify.com/v1/me/top/tracks?limit=50&time_range='+term[type],{
+            method:'GET',
+            headers:{
+                'Authorization':window.localStorage['token']
             }
+        })
+        res = await res.json()
+
+        if(res.error){
+            if(res.error.status == 401 || res.error.status == 400){
+                await getToken()
+                await getTopTracks(type);
+            }
+            return
         }
-        temp+='</div></div></div>'
-    }
 
-    $(`#listTrack${type}`).html(temp)
-    temp = ''
+        document.getElementById(`listTrack${type}`).innerHTML=''
 
-    for(i=0;i<res.items.length;i++){
-        temp+=`$('#listTrack${type}N${i}AlbumHolder').off().on('click',()=>{url = '${res.items[i].album.external_urls.spotify}';if(isMobile()){location.href = url}else{window.open(url)}});$('#listTrack${type}N${i}InfoHolder').off().on('click',()=>{url = '${res.items[i].external_urls.spotify}';if(isMobile()){location.href = url}else{window.open(url)}})\n`
-    }
-    eval(temp)
-}
+        for(i=0;i<res[items].length;i++){
+            let topEach = document[createElement]('div')
 
-const getTopArtists = async function(type){
-    if(type>2) return
+            topEach[setAttribute]('id',`listTrack${type}N${i}`)
+            topEach[setAttribute]('class','list')
 
-    let res,term,temp
-    term = ['long_term','medium_term','short_term']
-    res = await fetch('https://api.spotify.com/v1/me/top/artists?limit=50&time_range='+term[type],{
-        method:'GET',
-        headers:{
-            'Authorization':window.localStorage['token']
+            let listNumber = document[createElement]('div')
+
+            listNumber[setAttribute]('class','listNumber')
+            listNumber.innerHTML = i+1
+
+            let albumHolder = document[createElement]('div')
+
+            albumHolder[setAttribute]('id',`listTrack${type}N${i}AlbumHolder`)
+            albumHolder[setAttribute]('class','listTrackAlbumHolder')
+
+            let albumImg = document[createElement]('img')
+
+            albumImg[setAttribute]('id',`listTrack${type}N${i}Album`)
+            albumImg[setAttribute]('class','listTrackAlbum')
+            albumImg[setAttribute]('src',res[items][i].album.images[1].url)
+
+            let infoHolder = document[createElement]('div')
+
+            infoHolder[setAttribute]('id',`listTrack${type}N${i}InfoHolder`)
+            infoHolder[setAttribute]('class','listTrackInfoHolder')
+
+            let trackTitle = document[createElement]('div')
+
+            trackTitle[setAttribute]('class','listTrackTitle')
+            trackTitle.innerHTML = res[items][i].name
+
+            let trackArtist = document[createElement]('div')
+
+            for(j=0;j<res[items][i].artists.length;j++){
+                trackArtist.innerHTML+=res[items][i].artists[j].name
+
+                if(j!= res[items][i].artists.length-1){
+                    trackArtist.innerHTML+=', '
+                }
+            }
+
+            infoHolder[appendChild](trackTitle)
+            infoHolder[appendChild](trackArtist)
+
+            albumHolder[appendChild](albumImg)
+
+            topEach[appendChild](listNumber)
+            topEach[appendChild](albumHolder)
+            topEach[appendChild](infoHolder)
+
+            document.getElementById(`listTrack${type}`)[appendChild](topEach)
         }
-    })
-    res = await res.json()
 
-    if(res.error){
-        if(res.error.status == 401 || res.error.status == 400){
-            await getToken()
-            await getTopArtists(type)
+        for(i=0;i<res[items].length;i++){
+            $(`#listTrack${type}N${i}AlbumHolder`).off().on('click',{res,i},function(event){
+                let url = event.data.res.items[event.data.i].album.external_urls.spotify
+                if(isMobile()){
+                    location.href = url
+                }
+                else{
+                    window.open(url)
+                }
+            })
+            $(`#listTrack${type}N${i}InfoHolder`).off().on('click',{res,i},function(event){
+                let url = event.data.res.items[event.data.i].external_urls.spotify
+                if(isMobile()){
+                    location.href = url
+                }
+                else{
+                    window.open(url)
+                }
+            })
         }
-        return
     }
 
-    temp = ''
-    
-    for(i=0;i<res.items.length;i++){
-        temp+=`<div id="listArtist${type}N${i}" class="list"><div class="listNumber">${i+1}</div><div id="listArtist${type}N${i}ProfileHolder" class="listArtistProfileHolder"><img id="listArtist${type}N${i}Profile" class="listArtistProfile" src="${res.items[i].images[1].url}"></div><div id="listArtist${type}N${i}InfoHolder" class="listArtistInfoHolder"><p id="listArtist${type}N${i}Info" class="listArtistInfo">${res.items[i].name}</p></div></div>`
-    }
-    $(`#listArtist${type}`).html(temp)
-    temp = ''
+    const getTopArtists = async function(type){
+        let createElement = 'createElement' // best for minifier, but doesn't make it hard to code either
+        let setAttribute = 'setAttribute'
+        let items = 'items'
+        let appendChild = 'appendChild'  
+        if(type>2) return
 
-    for(i=0;i<res.items.length;i++){
-        temp+=`$('#listArtist${type}N${i}ProfileHolder').off().on('click',()=>{url = '${res.items[i].external_urls.spotify}';if(isMobile()){location.href = url}else{window.open(url)}});$('#listArtist${type}N${i}InfoHolder').off().on('click',()=>{url = '${res.items[i].external_urls.spotify}';if(isMobile()){location.href = url}else{window.open(url)}})\n`
-    }
-    eval(temp)
-}
+        let res,term,temp
+        term = ['long_term','medium_term','short_term']
+        res = await fetch('https://api.spotify.com/v1/me/top/artists?limit=50&time_range='+term[type],{
+            method:'GET',
+            headers:{
+                'Authorization':window.localStorage['token']
+            }
+        })
+        res = await res.json()
 
-getTopTracks(0)
-getTopTracks(1)
-getTopTracks(2)
-getTopArtists(0)
-getTopArtists(1)
-getTopArtists(2)
+        if(res.error){
+            if(res.error.status == 401 || res.error.status == 400){
+                await getToken()
+                await getTopArtists(type)
+            }
+            return
+        }
+        
+        document.getElementById(`listArtist${type}`).innerHTML = ''
+
+        for(i=0;i<res[items].length;i++){
+            let artistEach = document[createElement]('div')
+
+            artistEach[setAttribute]('id',`listArtist${type}N${i}`)
+            artistEach[setAttribute]('class','list')
+
+            let listNumber = document[createElement]('div')
+
+            listNumber[setAttribute]('class','listNumber')
+            listNumber.innerHTML = i+1
+
+            let profileHolder = document[createElement]('div')
+
+            profileHolder[setAttribute]('id',`listArtist${type}N${i}ProfileHolder`)
+            profileHolder[setAttribute]('class','listArtistProfileHolder')
+
+            let profile = document[createElement]('img')
+
+            profile[setAttribute]('id',`listArtist${type}N${i}Profile`)
+            profile[setAttribute]('class',`listArtistProfile`)
+            profile[setAttribute]('src',res[items][i].images[1].url)
+
+            let infoHolder = document[createElement]('div')
+
+            infoHolder[setAttribute]('id',`listArtist${type}N${i}InfoHolder`)
+            infoHolder[setAttribute]('class','listArtistInfoHolder')
+
+            let artistName = document[createElement]('p')
+
+            artistName[setAttribute]('id',`listArtist${type}N${i}Info`)
+            artistName[setAttribute]('class','listArtistInfo')
+            artistName.innerHTML = res[items][i].name
+
+            profileHolder[appendChild](profile)
+            infoHolder[appendChild](artistName)
+            artistEach[appendChild](listNumber)
+            artistEach[appendChild](profileHolder)
+            artistEach[appendChild](infoHolder)
+
+            document.getElementById(`listArtist${type}`)[appendChild](artistEach)
+        }
+
+        for(i=0;i<res[items].length;i++){
+            $(`#listArtist${type}N${i}ProfileHolder`).off().on('click',{res,i},function(event){
+                let url = event.data.res.items[event.data.i].external_urls.spotify
+                if(isMobile()){
+                    location.href = url
+                }
+                else{
+                    window.open(url)
+                }
+            })
+            $(`#listArtist${type}N${i}InfoHolder`).off().on('click',{res,i},function(event){
+                let url = event.data.res.items[event.data.i].external_urls.spotify
+                if(isMobile()){
+                    location.href = url
+                }
+                else{
+                    window.open(url)
+                }
+            })
+
+        }
+    }
+
+    getTopTracks(0)
+    getTopTracks(1)
+    getTopTracks(2)
+    getTopArtists(0)
+    getTopArtists(1)
+    getTopArtists(2)
+})(document)

bcc37d9f159d9cd7191757612e98b849dccf0633
Ihsan Rakhman <ihsan.maulana0501@gmail.com>
2021-08-17 09:28:55 +0700

    add style minify

--- minifier/uglify.js
+++ minifier/uglify.js
@@ -1,6 +1,8 @@
 const fs = require('fs')
 const uglify = require('uglify-js')
 const listUglify = JSON.parse(fs.readFileSync('./minifier/list_uglify.json'))
+const listUglifyStyle = JSON.parse(fs.readFileSync('./minifier/list_uglify_style.json'))
+const clean = require('clean-css')
 
 for(let bundle in listUglify){
     let fileData = {}
@@ -12,4 +14,18 @@ for(let bundle in listUglify){
     }
     let res = (uglify.minify(fileData,{mangle:{eval:true,toplevel:true}})).code
     fs.writeFileSync('./public/script_minify/'+bundle+'.js',res)
+}
+
+for(let bundle in listUglifyStyle){
+    let fileData = {}
+    for(file of listUglifyStyle[bundle]){
+        let fileName = file.split('/')
+        fileName = fileName[fileName.length-1]
+
+        fileData[fileName] = {
+            styles:fs.readFileSync(file,{encoding:'utf-8'})
+        }
+    }
+    let res = new clean().minify(fileData)
+    fs.writeFileSync('./public/style_minify/'+bundle+'.css',res.styles)
 }

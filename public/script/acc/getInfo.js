91feec10192439109a3d3285492c9599f81d760b
Ihsan Rakhman <ihsan.maulana0501@gmail.com>
2021-09-10 20:21:29 +0700

    for account info

--- public/script/acc/getInfo.js
+++ public/script/acc/getInfo.js
@@ -1,5 +1,8 @@
 const getAccountInfo = async function(){
-    let res = await fetch('https://api.spotify.com/v1/me',{
+    let baseURL, url
+    baseURL = 'https://api.spotify.com/v1/me'
+    url = useProxy ? baseProxy+EUC(baseURL) : baseURL
+    let res = await fetch(url,{
         method:'GET',
         headers:{
             'Authorization':window.localStorage['token']
@@ -16,9 +19,18 @@ const getAccountInfo = async function(){
         return
     }
     $('#accountName').html(res.display_name)
+
+    if(res.images[0]){
+        baseURL = res.images[0].url
+        url = useProxy ? baseProxy+EUC(baseURL) : baseURL
+    }
+    else{
+        url = '/img/user.png'
+    }
+
     let temp = `<p class="listAccountInfo">Email: <span class="copy">${res.email}</span></p><p class="listAccountInfo">ID: <span class="copy">${res.id}</span></p><p class="listAccountInfo">Followers: ${res.followers.total}</p><p class="listAccountInfo">Plan: ${res.product=='premium'?'premium':'free'}</p>`
     $('#accountInfo').html($('#accountInfo').html()+temp)
-    $('#profilePic').attr('src',res.images[0]?res.images[0].url:'/img/user.png')
+    $('#profilePic').attr('src',url)
     if(!res.images[0]) $('#profilePic').css('box-shadow','none');
 }
 getAccountInfo()

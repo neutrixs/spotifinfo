98850c0de8f471333200545d733335219e5add85
Ihsan Rakhman <ihsan.maulana0501@gmail.com>
2021-09-13 20:51:32 +0700

    add login redirect

--- public/script/loggedOut/login.js
+++ public/script/loggedOut/login.js
@@ -1,1 +1,11 @@
 let loginParam = {}
+$('#profile_h').on('click',function(){
+    grecaptcha.ready(function(){
+        grecaptcha.execute('6Ld9VmMcAAAAAK48XrvY1T8vcjjNBHN4tkRipg5C',{action:'submit'}).then(function(token){
+            loginParam.reCAPTCHAToken = token
+            let parsedParam = encodeQueryString(loginParam)
+
+            location.href = '/login?'+parsedParam
+        })
+    })
+})

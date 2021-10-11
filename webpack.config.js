903b5e750918950ceb93690d4eccbbce278cfeb8
Ihsan Rakhman <ihsan.maulana0501@gmail.com>
2021-10-11 21:24:42 +0700

    add he

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ module.exports = {
     mode:"production",
     entry: {
         "jQuery":"./src/script/base/jquery.js",
+        "he":"./src/script/base/he.js"
     },
     output: {
         filename:"[name].js",

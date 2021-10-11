05a66138ec0aeb21ec5a6e109636386d8d4c1fac
Ihsan Rakhman <ihsan.maulana0501@gmail.com>
2021-10-11 21:51:41 +0700

    let's see how it goes

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,28 @@
 const path = require('path')
+const TerserPlugin = require("terser-webpack-plugin")
 module.exports = {
     mode:"production",
     entry: {
         "jQuery":"./src/script/base/jquery.js",
-        "he":"./src/script/base/he.js"
+        "he":"./src/script/base/he.js",
+        "index":"./src/pages/index.js"
     },
     output: {
         filename:"[name].js",
         path: path.resolve(__dirname,"public")
+    },
+    optimization: {
+        minimizer:[
+            new TerserPlugin({
+                terserOptions:{
+                    mangle:true
+                    /*mangle:{
+                        properties:{
+                            regex:/(^_)[^\s]+/
+                        }
+                    }*/
+                }
+            })
+        ]
     }
 }

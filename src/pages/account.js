efae754da367602b3c45668959bee6e3d1b0065a
Ihsan Rakhman <ihsan.maulana0501@gmail.com>
2021-10-12 16:43:56 +0700

    add basic account page

---
+++ src/pages/account.js
@@ -0,0 +1,20 @@
+import {
+    globalVar,
+    getToken,
+    baseStart,
+    unsetHidden
+} from '../script/base/base'
+
+import { themeStart } from '../script/acc/theme'
+
+import $ from 'jquery'
+import he from 'he'
+import '../style/base.css'
+import '../style/acc/account.css'
+
+window.$ = $
+window.he = he
+
+unsetHidden()
+baseStart()
+themeStart()

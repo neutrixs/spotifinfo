f100410b443d430565a2e38a6ed4152188e0b6e9
Ihsan Rakhman <ihsan.maulana0501@gmail.com>
2021-10-13 21:31:25 +0700

    using development client for development

--- config_for_self.js
+++ config_for_self.js
@@ -1,7 +1,9 @@
 module.exports = {
     scope:'user-read-email user-read-private user-read-recently-played user-top-read user-read-playback-position user-read-playback-state user-read-currently-playing',
-    client_id:'170452a430ad4323beb60b9e871dcf77',
-    client_secret:'57f4f7cc733b4950a274c422e4fa0ea0',
+    client_id_prod:'170452a430ad4323beb60b9e871dcf77',
+    client_secret_prod:'57f4f7cc733b4950a274c422e4fa0ea0',
+    client_id:'542d072b2d344ea08f1e7944173e9453',
+    client_secret:'49a288749a9f4a65b9583eb1f9595a75',
     redirect_uri:'http://192.168.1.50/callback',
     firestore:false,
     pass:'KontolBesar69420'

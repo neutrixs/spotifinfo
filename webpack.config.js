9702f3463fdd695741ac31b1a7beaea3965efce3
Ihsan Rakhman <ihsan.maulana0501@gmail.com>
2021-10-15 14:17:19 +0700

    support ts too

--- webpack.config.js
+++ webpack.config.js
@@ -45,6 +45,11 @@ const config = {
                 test: /\.css$/i,
                 use: ["style-loader", "css-loader"],
             },
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
         ]
     },
     plugins:[

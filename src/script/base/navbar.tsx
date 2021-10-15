23c95eec73c277720b1a2de8a8536ce38d47a831
Ihsan Rakhman <ihsan.maulana0501@gmail.com>
2021-10-15 19:23:07 +0700

    add isloggedout

--- src/script/base/navbar.tsx
+++ src/script/base/navbar.tsx
@@ -1,8 +1,17 @@
 import * as React from 'react';
+import { Component } from 'react';
 import '../../style/base/navbar.css'
 
-export class Navbar extends React.Component {
+interface navbarParam {
+    isLoggedOut:boolean
+}
+
+export class Navbar extends Component<navbarParam> {
+    constructor(props:any) {
+        super(props)
+    }
     render(){
+        console.log(this.props)
         return(
             <div id="navBar"></div>
         )
--- src/script/index.tsx
+++ src/script/index.tsx
@@ -3,13 +3,13 @@ import * as ReactDOM from 'react-dom';
 import { Navbar } from './base/navbar'
 import '../style/base/base.css'
 
-function isLoggedOut(){
+function isLoggedOut():boolean{
     return document.cookie.indexOf('state=') == -1 || document.cookie.indexOf('uname=') == -1
 }
 
 ReactDOM.render(
     <>
-        <Navbar/>
+        <Navbar isLoggedOut={isLoggedOut()}/>
         <h1>Hello, world!</h1>
     </>,
     document.getElementById('root')

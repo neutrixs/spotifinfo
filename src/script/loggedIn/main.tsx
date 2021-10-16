774fedbe076915995a30d4460e95e6309941090f
Ihsan Rakhman <ihsan.maulana0501@gmail.com>
2021-10-16 18:26:47 +0700

    add mobile desktop (not finished)

--- src/script/loggedIn/main.tsx
+++ src/script/loggedIn/main.tsx
@@ -1,10 +1,17 @@
 import * as React from 'react';
 import { NowPlaying } from './nowPlaying';
 import '../../style/loggedIn/main.css'
+import { mobileDesktopStart, mobileDesktopStop } from './mobileDesktop';
 
 export class LoggedInMain extends React.Component{
     constructor(props:any){
         super(props)
+    }
+    componentDidMount(){
+        mobileDesktopStart()
+    }
+    componentWillUnmount(){
+        mobileDesktopStop()
     }
     render(){
         return(
---
+++ src/script/loggedIn/mobileDesktop.tsx
@@ -0,0 +1,33 @@
+const mobile = function(){
+    document.getElementById('page').classList.add('pageMobile')
+}
+const desktop = function(){
+    document.getElementById('page').classList.remove('pageMobile')
+}
+
+let changeAt = 44.5
+const bodyFontSize = window.getComputedStyle(document.body).getPropertyValue('font-size')
+if(window.innerWidth / parseFloat(bodyFontSize) < changeAt) mobile();
+//document.getElementById('page').classList.remove('none')
+//document.getElementById('page').classList.add('ts300ms')
+
+const handler = function(){
+    let width = window.innerWidth;
+        
+    let emWidth = width / parseFloat(bodyFontSize)
+    if(emWidth < changeAt){
+        mobile()
+    }
+    else{
+        desktop()
+    }
+}
+
+const mobileDesktopStart = function(){
+    window.addEventListener('resize',handler)
+}
+const mobileDesktopStop = function(){
+    window.removeEventListener('resize',handler)
+}
+
+export { mobileDesktopStart, mobileDesktopStop }

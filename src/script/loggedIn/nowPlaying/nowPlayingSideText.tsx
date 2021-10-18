75dfe30f8ba61fdf4bf6dd0ea5c46272946f0454
Ihsan Rakhman <ihsan.maulana0501@gmail.com>
2021-10-18 16:25:21 +0700

    working side text

--- src/script/loggedIn/nowPlaying/nowPlayingSideText.tsx
+++ src/script/loggedIn/nowPlaying/nowPlayingSideText.tsx
@@ -1,8 +1,14 @@
 import {NowPlaying} from './nowPlaying'
 
 let listener = function(this:NowPlaying){
-    const imgHolderWidth = 21.5 * parseFloat(window.getComputedStyle(document.body).getPropertyValue('font-size'))
-    const nowPlayingWidth = document.getElementById('nowPlaying').clientWidth
+    const emSize = parseFloat(window.getComputedStyle(document.body).getPropertyValue('font-size'))
+
+    const imgHolderWidth = 21.5 * emSize
+
+    // if it's a mobile version, calculate the 3em (now playing padding), else, calculate the 3em + left + right margin (which is 10em each)
+    // why? because using clientWidth will return 0 if it's just being rendered
+    const nowPlayingWidth = window.innerWidth - (this.props.classNowPlayingMobile ? 3 : 23 * emSize)
+
     const side = imgHolderWidth / nowPlayingWidth < 0.5
 
     this.setState({

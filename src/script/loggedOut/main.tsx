7f028626cbdcd4c293ccd81d0f2ce0b3ba9217c9
Ihsan Rakhman <ihsan.maulana0501@gmail.com>
2021-10-19 19:38:07 +0700

    fix css a bit

--- src/script/loggedOut/main.tsx
+++ src/script/loggedOut/main.tsx
@@ -9,18 +9,20 @@ export class LoggedOutMain extends React.Component {
     render(){
         return(
             <div id="page">
-                <div id="kingHolder">
-                    <img id="king" src="/img/kinghd.png" />
-                </div>
-                <div id="infoHolder">
-                    <p id="title">Spotifinfo</p>
-                    <p id="subTitle">What you can do with this:</p>
-                    <ul id="listYouCanDo">
-                        <li>See your now playing</li>
-                        <li>See your listening history</li>
-                        <li>See your top tracks and artists</li>
-                        <li>Info about your Spotify account</li>
-                    </ul>
+                <div id="loggedOutHolder">
+                    <div id="kingHolder">
+                        <img id="king" src="/img/kinghd.png" />
+                    </div>
+                    <div id="infoHolder">
+                        <p id="title">Spotifinfo</p>
+                        <p id="subTitle">What you can do with this:</p>
+                        <ul id="listYouCanDo">
+                            <li>See your now playing</li>
+                            <li>See your listening history</li>
+                            <li>See your top tracks and artists</li>
+                            <li>Info about your Spotify account</li>
+                        </ul>
+                    </div>
                 </div>
             </div>
         )

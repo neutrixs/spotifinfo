7c89f56bb3a24aa3f664aeaad5fb3056c4f71701
Ihsan Rakhman <ihsan.maulana0501@gmail.com>
2021-10-19 19:29:26 +0700

    add logged out info

--- src/script/loggedOut/main.tsx
+++ src/script/loggedOut/main.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import '../../style/loggedOut/main.css'
 
 export class LoggedOutMain extends React.Component {
     constructor(props:{}){
@@ -7,7 +8,21 @@ export class LoggedOutMain extends React.Component {
 
     render(){
         return(
-            <div id="page" />
+            <div id="page">
+                <div id="kingHolder">
+                    <img id="king" src="/img/kinghd.png" />
+                </div>
+                <div id="infoHolder">
+                    <p id="title">Spotifinfo</p>
+                    <p id="subTitle">What you can do with this:</p>
+                    <ul id="listYouCanDo">
+                        <li>See your now playing</li>
+                        <li>See your listening history</li>
+                        <li>See your top tracks and artists</li>
+                        <li>Info about your Spotify account</li>
+                    </ul>
+                </div>
+            </div>
         )
     }
 }

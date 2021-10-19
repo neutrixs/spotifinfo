86144c9448d0b1efe7a272749dde416a6780f23f
Ihsan Rakhman <ihsan.maulana0501@gmail.com>
2021-10-19 17:42:21 +0700

    redirect if not logged in

--- src/script/base/routeGenerate.tsx
+++ src/script/base/routeGenerate.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Switch, Route } from 'react-router-dom'
+import { Switch, Route, Redirect } from 'react-router-dom'
 import { LoggedInMain } from '../loggedIn/main'
 import { LoggedOutMain } from '../loggedOut/main'
 
@@ -28,6 +28,16 @@ export class RouteGenerate extends React.Component<routeGenerateParam>{
             <Switch>
                 <Route exact path="/">
                     {this.homepage()}
+                </Route>
+                <Route exact path="/top_tracks">
+                    {
+                        this.props.isLoggedOut ? <Redirect to="/" /> : null
+                    }
+                </Route>
+                <Route exact path="/account">
+                    {
+                        this.props.isLoggedOut ? <Redirect to="/" /> : null
+                    }
                 </Route>
             </Switch>
         )

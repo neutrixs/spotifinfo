8014ae39203088fa7a3b9c93d0877a33273d5163
Ihsan Rakhman <ihsan.maulana0501@gmail.com>
2021-10-20 12:37:20 +0700

    add type selector

--- src/script/top/selector.tsx
+++ src/script/top/selector.tsx
@@ -19,6 +19,26 @@ export default class Selector extends React.Component<props>{
                 <p id="typeName">
                     Top {this.props.selectedType === 0 ? 'Tracks' : 'Artists'}
                 </p>
+                <div id="typeSelector">
+                    <div 
+                        className={
+                            'typeSelectorEach '+
+                            (this.props.selectedType === 0 ? 'selected ' : '')
+                        }
+                        onClick={()=>this.props.setSelectedType(0)}
+                    >
+                        <span>Top Tracks</span>
+                    </div>
+                    <div 
+                        className={
+                            'typeSelectorEach '+
+                            (this.props.selectedType === 1 ? 'selected ' : '')
+                        }
+                        onClick={()=>this.props.setSelectedType(1)}
+                    >
+                        <span>Top Artists</span>
+                    </div>
+                </div>
             </div>
         )
     }

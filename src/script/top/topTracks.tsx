686815d9ac0d436f01cad9bcf23e77556843ece7
Ihsan Rakhman <ihsan.maulana0501@gmail.com>
2021-10-20 12:10:47 +0700

    add top tracks and artists classes

---
+++ src/script/top/topTracks.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+interface props{
+    selectedType:0|1
+    selectedRange:0|1|2
+    Range:0|1|2
+}
+
+export default class TopTracks extends React.Component<props>{
+    constructor(props:props){
+        super(props)
+    }
+}

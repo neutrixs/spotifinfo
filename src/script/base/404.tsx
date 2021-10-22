7578d6bcddf9595252df9b9b451339ce46d3a511
Ihsan Rakhman <ihsan.maulana0501@gmail.com>
2021-10-22 09:29:19 +0700

    nice 404 page

--- src/script/base/404.tsx
+++ src/script/base/404.tsx
@@ -8,12 +8,12 @@ export default class Page404 extends React.Component{
 
     render(){
         return(
-            <div
-                style={{
-                    fontFamily:'uni-sans'
-                }}
-            >
-                testing
+            <div id="holderAll">
+                <div id="holder404">
+                    <p id="text404">404</p>
+                    <p id="notFound">NOT FOUND</p>
+                    <p id="info404">THE REQUESTED RESOURCE {'\u00a0'} {window.location.pathname.toUpperCase()} {'\u00a0'} COULD NOT BE FOUND</p>
+                </div>
             </div>
         )
     }

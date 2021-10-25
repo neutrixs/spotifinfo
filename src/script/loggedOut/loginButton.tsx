755043c407705cb22f230bdccb334ebae8fb695c
Ihsan Rakhman <ihsan.maulana0501@gmail.com>
2021-10-25 21:50:24 +0700

    login button sass

--- src/script/loggedOut/loginButton.tsx
+++ src/script/loggedOut/loginButton.tsx
@@ -1,12 +1,12 @@
 import * as React from 'react';
 import {Navbar} from '../base/navbar'
-import '../../style/loggedOut/loginNavbar.css'
+import '../../style/loggedOut/loginNavbar.scss'
 import * as spotifyLogo from '../../img/spotify_logo.png'
 
 export default function loginButton(this:Navbar):JSX.Element{
     return (
         <a id="loginHolder" href={this.login()}>
-            <img id="spotifyLogo" src={spotifyLogo} />
+            <img src={spotifyLogo} />
             <span 
                 style={{
                     lineHeight:'1em',

5bad5b680d97d0bbfb2010e57712b0f0fac40c15
Ihsan Rakhman <ihsan.maulana0501@gmail.com>
2021-10-25 15:20:04 +0700

    test

--- webpack.config.js
+++ webpack.config.js
@@ -52,6 +52,14 @@ const config = ()=> ({
                 ],
             },
             {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    "css-loader",
+                    "sass-loader"
+                ]
+            },
+            {
                 test: /\.tsx?$/,
                 use: 'ts-loader',
                 exclude: /node_modules/,

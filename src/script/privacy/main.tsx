ce33b94b1db793ba45e200d6a147d2d7a1c2b940
Ihsan Rakhman <ihsan.maulana0501@gmail.com>
2021-11-13 19:35:54 +0700

    fetch external md

--- src/script/privacy/main.tsx
+++ src/script/privacy/main.tsx
@@ -1,16 +1,34 @@
 import * as React from 'react'
 import ReactMarkdown from 'react-markdown'
+import * as privacyPolicyText from './privacy.md'
 
-export default class Privacy extends React.Component {
+interface states{
+    textData:string
+}
+
+export default class Privacy extends React.Component<{},states> {
     constructor(props:{}){
         super(props)
+
+        this.state = {
+            textData:'# Privacy Policy'
+        }
+    }
+
+    async componentDidMount(){
+        let data = await fetch(privacyPolicyText)
+        let text = await data.text()
+
+        this.setState({
+            textData:text
+        })
     }
 
     render(){
         return(
             <div id="privacyHolder" style={{padding:'1.5em'}}>
                 <ReactMarkdown>
-                    # Privacy Policy
+                    {this.state.textData}
                 </ReactMarkdown>
             </div>
         )

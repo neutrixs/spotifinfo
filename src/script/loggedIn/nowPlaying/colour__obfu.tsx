e2139feb4394e6fb73fa934d5efb6faf7494e420
Ihsan Rakhman <ihsan.maulana0501@gmail.com>
2021-11-16 22:10:53 +0700

    why wont you work??

--- src/script/loggedIn/nowPlaying/colour__obfu.tsx
+++ src/script/loggedIn/nowPlaying/colour__obfu.tsx
@@ -1,15 +1,18 @@
 import {NowPlaying} from './nowPlaying'
-import {indexMostSaturated,autoAdjustLightness} from './colourModule__obfu'
+import {indexMostSaturated,autoAdjust} from './colourModule__obfu'
 const colorthief = new window.ColorThief()
+
+type rgbArray = [number,number,number]
+
 function colour(this:NowPlaying){
     const img = document.getElementById('albumArt')
-    const palette:Array<number[]> = colorthief.getPalette(img,5)
+    const palette:rgbArray[] = colorthief.getPalette(img,5)
     const indexNo = indexMostSaturated([...palette])
     const saturatedPalette = palette[indexNo]
 
     let finalPalette = [saturatedPalette,saturatedPalette]
-    finalPalette[0] = autoAdjustLightness(finalPalette[0],0.35,0.45)
-    finalPalette[1] = autoAdjustLightness(finalPalette[1],0.7,0.8)
+    finalPalette[0] = autoAdjust(finalPalette[0],0.4,0.02)
+    finalPalette[1] = autoAdjust(finalPalette[1],0.75,0.02)
 
     this.setState({
         palette:finalPalette

e8321b63d4165b25384a61abe2637d5d22f8ff20
Ihsan Rakhman <ihsan.maulana0501@gmail.com>
2021-12-29 18:48:46 +0700

    add declaration file for colorthief

---
+++ src/pages/loggedIn/types/colorthief.d.ts
@@ -0,0 +1,9 @@
+// https://github.com/lokesh/color-thief/issues/188#issuecomment-871655396
+
+declare module 'colorthief' {
+    type Color = [number, number, number];
+    export default class ColorThief {
+        getColor: (img: HTMLImageElement | null) => Color;
+        getPalette: (img: HTMLImageElement | null) => Color[];
+    }
+}

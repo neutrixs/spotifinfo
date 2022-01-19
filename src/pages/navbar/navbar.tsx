cba444896879197e641d5e0e5f734605617ec8e9
Ihsan Rakhman <ihsan.maulana0501@gmail.com>
2022-01-19 20:48:39 +0700

    bit navbar new

---
+++ src/pages/navbar/navbar.tsx
@@ -0,0 +1,44 @@
+import * as React from 'react'
+import { NavLink } from 'react-router-dom'
+
+import './navbar.scss'
+
+interface props {
+    isLoggedOut: boolean
+    toggleTheme: () => void
+    isDark: boolean
+}
+
+export default function Navbar({ isLoggedOut, toggleTheme, isDark }: props) {
+    function topTracksText() {
+        const element = (
+            <NavLink to="/top_tracks">
+                <span>Top Tracks/Artists</span>
+            </NavLink>
+        )
+
+        return !isLoggedOut ? element : null
+    }
+
+    function privacyPolicyText() {
+        const element = (
+            <NavLink to="/privacy">
+                <span>Privacy Policy</span>
+            </NavLink>
+        )
+
+        return isLoggedOut ? element : null
+    }
+
+    return (
+        <div id="navbar">
+            <div id="leftNavbar">
+                <NavLink to="/">
+                    <span>Home</span>
+                </NavLink>
+                {privacyPolicyText()}
+                {topTracksText()}
+            </div>
+        </div>
+    )
+}

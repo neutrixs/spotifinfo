c559125f0e83fbab43d9200d03bd81e36b80953e
Ihsan Rakhman <ihsan.maulana0501@gmail.com>
2022-01-21 17:14:15 +0700

    add main page out page

--- src/pages/mainPageOut/mainPageOut.tsx
+++ src/pages/mainPageOut/mainPageOut.tsx
@@ -1,10 +1,30 @@
 import * as React from 'react'
 
+import detectWEBPSupport from '../other/detectWEBPSupport'
+
+import './mainPageOut.scss'
+
+import * as turtle from '../../img/kinghd.png'
+import * as turtleWEBP from '../../img/kinghd.webp'
+
 export default function MainPageOut() {
+    const supportWEBP = detectWEBPSupport()
+
     return (
         <div id="mainPageOut">
-            <div>
-                <p>Lorem Ipsum dolor sit amet consectetur adipiscing elit</p>
+            <div className="content">
+                <img src={supportWEBP ? turtleWEBP : turtle} />
+                <div>
+                    <p style={{ fontSize: '2em', fontWeight: 600 }}>Spotifinfo</p>
+                    <p style={{ fontSize: '2em' }}>What you can do with this:</p>
+
+                    <ul style={{ fontSize: '2em' }}>
+                        <li>See your now playing</li>
+                        <li>See your listening history</li>
+                        <li>See your top tracks and artists</li>
+                        <li>Info about your Spotify account</li>
+                    </ul>
+                </div>
             </div>
         </div>
     )

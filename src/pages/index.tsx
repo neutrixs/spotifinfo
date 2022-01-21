e05ac5877f313b74c7050922a72f1f8ebc27e9cb
Ihsan Rakhman <ihsan.maulana0501@gmail.com>
2022-01-21 13:49:27 +0700

    add mainPageOut routing

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,11 @@
 import * as React from 'react'
-import { useState, useEffect } from 'react'
+import { useState, useEffect, lazy, Suspense } from 'react'
 import { render } from 'react-dom'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
 import Navbar from './navbar/navbar'
+
+const MainPageOut = lazy(() => import('./mainPageOut/mainPageOut'))
 
 import './base.scss'
 
@@ -19,10 +21,27 @@ function Main() {
         localStorage.setItem('isDark', isDark.toString())
     }, [])
 
+    function getMainPageRouting() {
+        if (isLoggedOut) {
+            return (
+                <Suspense fallback={null}>
+                    <MainPageOut />
+                </Suspense>
+            )
+        }
+
+        //TODO: add mainPageIn
+    }
+
     return (
         <BrowserRouter>
             <div className={'fake ' + (!isDark ? 'fakeLight' : '')}>
                 <Navbar isLoggedOut={isLoggedOut} toggleTheme={toggleTheme} isDark={isDark} />
+                <Switch>
+                    <Route exact path="/">
+                        {getMainPageRouting()}
+                    </Route>
+                </Switch>
             </div>
         </BrowserRouter>
     )
---
+++ src/pages/mainPageOut/mainPageOut.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react'
+
+export default function MainPageOut() {
+    return (
+        <div id="mainPageOut">
+            <div>
+                <p>Lorem Ipsum dolor sit amet consectetur adipiscing elit</p>
+            </div>
+        </div>
+    )
+}

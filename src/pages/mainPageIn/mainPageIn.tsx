29056eeb3f8ded0f8957d6ab82ac6c96862c8a4b
Ihsan Rakhman <ihsan.maulana0501@gmail.com>
2022-01-22 11:37:39 +0700

    add mobile desktop handler for mainPageIn

--- src/pages/mainPageIn/mainPageIn.tsx
+++ src/pages/mainPageIn/mainPageIn.tsx
@@ -1,4 +1,7 @@
 import * as React from 'react'
+import { useState, useEffect } from 'react'
+
+import { mdHandler, mdHandlerBoolean } from '../other/mdHandler'
 
 import './mainPageIn.scss'
 
@@ -7,9 +10,23 @@ interface props {
 }
 
 export default function MainPageIn({ isDark }: props) {
+    const [isMobile, setIsMobile] = useState<boolean>(mdHandlerBoolean())
+
+    useEffect(() => {
+        window.addEventListener('resize', callMDHandler)
+
+        return function cleanup() {
+            window.removeEventListener('resize', callMDHandler)
+        }
+    }, [])
+
+    function callMDHandler() {
+        mdHandler(setIsMobile)
+    }
+
     return (
         <div id="mainPageIn">
-            <div className="content"></div>
+            <div className="content">tet</div>
         </div>
     )
 }

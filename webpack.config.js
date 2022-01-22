2680fec1f9dda04c914db4ed8715f824df0feb65
Ihsan Rakhman <ihsan.maulana0501@gmail.com>
2022-01-22 10:22:30 +0700

    code formatting

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,15 @@
 const path = require('path')
-const TerserPlugin = require("terser-webpack-plugin")
-const CopyPlugin = require("copy-webpack-plugin")
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
-const WebpackObfuscator = require('webpack-obfuscator');
+const TerserPlugin = require('terser-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
+const WebpackObfuscator = require('webpack-obfuscator')
 let devMode = false
 
-module.exports = function(env,argv){
+module.exports = function (env, argv) {
     console.log(argv)
-    devMode = argv.mode == "development"
+    devMode = argv.mode == 'development'
 
     const nameOrContentHash = devMode ? '[name]' : '[contenthash]'
 
@@ -17,51 +17,44 @@ module.exports = function(env,argv){
 }
 
 const mangleNormal = {
-    properties:{
-        regex:/(^_)[^\s]+/
-    }
+    properties: {
+        regex: /(^_)[^\s]+/,
+    },
 }
 
-const config = (nameOrContentHash)=> ({
-    mode: devMode ? "development" : "production",
+const config = nameOrContentHash => ({
+    mode: devMode ? 'development' : 'production',
     entry: {
-        "index":'./src/pages/index.tsx'
+        index: './src/pages/index.tsx',
     },
     output: {
-        filename:`assets/${nameOrContentHash}.js`,
-        path: path.resolve(__dirname,"public"),
-        clean:true
+        filename: `assets/${nameOrContentHash}.js`,
+        path: path.resolve(__dirname, 'public'),
+        clean: true,
     },
-    target: ['web','es6'],
+    target: ['web', 'es6'],
     optimization: {
-        minimizer:[
+        minimizer: [
             new TerserPlugin({
-                terserOptions:{
+                terserOptions: {
                     /*mangle:{
                         properties:true,
                     }*/
-                    mangle:devMode ? false : mangleNormal
-                }
+                    mangle: devMode ? false : mangleNormal,
+                },
             }),
-            new CssMinimizerPlugin()
-        ]
+            new CssMinimizerPlugin(),
+        ],
     },
-    module:{
-        rules:[
+    module: {
+        rules: [
             {
                 test: /\.css$/i,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    "css-loader"
-                ],
+                use: [MiniCssExtractPlugin.loader, 'css-loader'],
             },
             {
                 test: /\.s[ac]ss$/i,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    "css-loader",
-                    "sass-loader"
-                ]
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
             },
             {
                 test: /\.tsx?$/,
@@ -73,56 +66,58 @@ const config = (nameOrContentHash)=> ({
                 loader: 'file-loader',
                 options: {
                     name: `assets/${nameOrContentHash}.[ext]`,
-                    esModule:false
+                    esModule: false,
                 },
             },
-            (!devMode?{
-                test: /__obfu\.[tj]sx?$/,
-                enforce: 'post',
-                use: {
-                    loader: WebpackObfuscator.loader,
-                    options: {
-                        deadCodeInjection: true,
-                        deadCodeInjectionThreshold: 1,
-                        controlFlowFlattening: true,
-                        controlFlowFlatteningThreshold: 0.2,
-                        domainLock: ['192.168.1.50','spotifinfo.neutrix-web.xyz'],
-                        domainLockRedirectUrl: 'https://youtu.be/dQw4w9WgXcQ?autoplay=true' //because why not :D
-                    }
-                }
-            }:{})
-        ]
+            !devMode
+                ? {
+                      test: /__obfu\.[tj]sx?$/,
+                      enforce: 'post',
+                      use: {
+                          loader: WebpackObfuscator.loader,
+                          options: {
+                              deadCodeInjection: true,
+                              deadCodeInjectionThreshold: 1,
+                              controlFlowFlattening: true,
+                              controlFlowFlatteningThreshold: 0.2,
+                              domainLock: ['192.168.1.50', 'spotifinfo.neutrix-web.xyz'],
+                              domainLockRedirectUrl: 'https://youtu.be/dQw4w9WgXcQ?autoplay=true', //because why not :D
+                          },
+                      },
+                  }
+                : {},
+        ],
     },
-    plugins:[
+    plugins: [
         new CopyPlugin({
-            patterns:[
+            patterns: [
                 {
-                    from:'./src/favicon.ico',
-                    to:'./favicon.ico'
-                }
-            ]
+                    from: './src/favicon.ico',
+                    to: './favicon.ico',
+                },
+            ],
         }),
         new HtmlWebpackPlugin({
-            template:'./src/index.html',
-            filename:devMode ? 'index.html' : 'meaningOfLife.html',
-            publicPath:'/'
+            template: './src/index.html',
+            filename: devMode ? 'index.html' : 'meaningOfLife.html',
+            publicPath: '/',
         }),
         new MiniCssExtractPlugin({
-            filename:`assets/${nameOrContentHash}.css`
-        })
+            filename: `assets/${nameOrContentHash}.css`,
+        }),
     ],
     resolve: {
         extensions: ['.tsx', '.ts', '.js'],
     },
     devServer: {
-        host:'192.168.1.50',
+        host: '192.168.1.50',
         static: './public',
-        hot:true,
-        historyApiFallback:true,
-        allowedHosts: ['192.168.1.50','localhost'],
-        proxy:{
-            '/gettoken':'http://192.168.1.50',
-            '/login':'http://192.168.1.50'
-        }
-    }
+        hot: true,
+        historyApiFallback: true,
+        allowedHosts: ['192.168.1.50', 'localhost'],
+        proxy: {
+            '/gettoken': 'http://192.168.1.50',
+            '/login': 'http://192.168.1.50',
+        },
+    },
 })

9e7d8b6f737af77e8fea0d8bed8ae925e6ee7986
Ihsan Rakhman <ihsan.maulana0501@gmail.com>
2022-01-23 21:13:16 +0700

    colorthief declaration

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,13 @@
-declare module '*.png';
-declare module '*.jpg';
-declare module '*.svg';
-declare module '*.webp';
-declare module '*.otf';
-declare module '*.md';
+declare module '*.png'
+declare module '*.jpg'
+declare module '*.svg'
+declare module '*.webp'
+declare module '*.otf'
+declare module '*.md'
+declare module 'colorthief' {
+    type Color = [number, number, number]
+    export default class ColorThief {
+        getColor: (img: HTMLImageElement | null) => Color
+        getPalette: (img: HTMLImageElement | null) => Color[]
+    }
+}

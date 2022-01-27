7b2d9eec1dc6bf9d3b29838cd08ff753f8a1f8fc
Ihsan Rakhman <ihsan.maulana0501@gmail.com>
2022-01-27 13:45:30 +0700

    add type selector

---
+++ src/pages/topPage/selectors/typeSelector.tsx
@@ -0,0 +1,56 @@
+import * as React from 'react'
+
+import { typeSelector as typeSelectorEnum } from '../topPage'
+
+import './typeSelector.scss'
+
+interface props {
+    isDark: boolean
+    selectedType: typeSelectorEnum
+    setSelectedType: React.Dispatch<React.SetStateAction<typeSelectorEnum>>
+}
+
+export default function TypeSelector({ isDark, selectedType, setSelectedType }: props) {
+    function onClick(enumToSet: typeSelectorEnum, e?: React.KeyboardEvent<HTMLDivElement>) {
+        if (e && e?.key !== 'Enter') return
+
+        setSelectedType(enumToSet)
+    }
+
+    return (
+        <div id="typeSelector">
+            <div
+                className={
+                    (selectedType == typeSelectorEnum.tracks ? 'selected ' : '') +
+                    (selectedType == typeSelectorEnum.tracks && !isDark ? 'selectedLight ' : '')
+                }
+                role="button"
+                tabIndex={0}
+                onClick={() => {
+                    onClick(typeSelectorEnum.tracks)
+                }}
+                onKeyPress={e => {
+                    onClick(typeSelectorEnum.tracks, e)
+                }}
+            >
+                <span>Top Tracks</span>
+            </div>
+            <div
+                className={
+                    (selectedType == typeSelectorEnum.artists ? 'selected ' : '') +
+                    (selectedType == typeSelectorEnum.artists && !isDark ? 'selectedLight ' : '')
+                }
+                role="button"
+                tabIndex={0}
+                onClick={() => {
+                    onClick(typeSelectorEnum.artists)
+                }}
+                onKeyPress={e => {
+                    onClick(typeSelectorEnum.artists, e)
+                }}
+            >
+                <span>Top Artists</span>
+            </div>
+        </div>
+    )
+}
--- src/pages/topPage/topPage.tsx
+++ src/pages/topPage/topPage.tsx
@@ -1,4 +1,11 @@
 import * as React from 'react'
+import { useState, useEffect } from 'react'
+
+import { mdHandlerBoolean, mdHandler } from '../other/mdHandler'
+
+import TypeSelector from './selectors/typeSelector'
+
+import './topPage.scss'
 
 interface props {
     isDark: boolean
@@ -16,9 +23,29 @@ enum rangeSelector {
 }
 
 export default function TopPage({ isDark }: props) {
+    const [selectedType, setSelectedType] = useState(typeSelector.tracks)
+    const [isMobile, setIsMobile] = useState(mdHandlerBoolean())
+
+    useEffect(() => {
+        window.addEventListener('resize', callMdHandler)
+
+        return function cleanup() {
+            window.removeEventListener('resize', callMdHandler)
+        }
+    })
+
+    function callMdHandler() {
+        mdHandler(setIsMobile)
+    }
+
     return (
-        <div id="topPage">
-            <div id="content"></div>
+        <div id="topPage" className={isMobile ? 'mobile' : ''}>
+            <div id="content">
+                <p id="title">Top {selectedType == typeSelector.tracks ? 'Tracks' : 'Artists'}</p>
+                <div id="selectorsHolder">
+                    <TypeSelector isDark={isDark} selectedType={selectedType} setSelectedType={setSelectedType} />
+                </div>
+            </div>
         </div>
     )
 }

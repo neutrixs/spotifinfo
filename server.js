790fdee418a3dc746050b71c808a8f28462c8f38
Ihsan Rakhman <ihsan.maulana0501@gmail.com>
2022-02-04 22:54:10 +0700

    enable caching + formatting caching is actually pretty useful tbh

--- server.js
+++ server.js
@@ -1,32 +1,32 @@
-const express = require('express');
-const app = express();
-const cookie_parser = require('cookie-parser');
-const data = require(process.argv.includes('--devmode') ? './config_for_self' : './config' );
-const admin = require('firebase-admin');
-const serviceAccount = require('./SECRET/accountkey.json');
+const express = require('express')
+const app = express()
+const cookie_parser = require('cookie-parser')
+const data = require(process.argv.includes('--devmode') ? './config_for_self' : './config')
+const admin = require('firebase-admin')
+const serviceAccount = require('./SECRET/accountkey.json')
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
-});
-const db = admin.firestore();
+    credential: admin.credential.cert(serviceAccount),
+})
+const db = admin.firestore()
 
 require('./checker')()
 
-if(process.argv.includes('--devmode')) {
-    app.use(express.static(__dirname+'/public'))
+if (process.argv.includes('--devmode')) {
+    app.use(express.static(__dirname + '/public'))
 }
 
-app.disable('view cache')
+// app.disable('view cache')
 
 app.use(cookie_parser())
-app.use(express.urlencoded({extended:true}))
-app.engine('html', require('ejs').renderFile);
-app.set('view engine', 'html');
-app.set('views', __dirname+'/public')
+app.use(express.urlencoded({ extended: true }))
+app.engine('html', require('ejs').renderFile)
+app.set('view engine', 'html')
+app.set('views', __dirname + '/public')
 
-app.get(/^\/(|top_tracks|account|privacy)[\/]*$/,(req,res)=>{
-    const newPath = req.path.replace(/\/+/g,'/')
+app.get(/^\/(|top_tracks|account|privacy)[\/]*$/, (req, res) => {
+    const newPath = req.path.replace(/\/+/g, '/')
 
-    if(req.path != newPath){
+    if (req.path != newPath) {
         res.redirect(newPath)
         return
     }
@@ -34,34 +34,33 @@ app.get(/^\/(|top_tracks|account|privacy)[\/]*$/,(req,res)=>{
     res.render('meaningOfLife.html')
 })
 
-app.get(/^\//,(req,res)=>{
-
+app.get(/^\//, (req, res) => {
     let isLoggedOut = !req.cookies['state'] || !req.cookies['uname']
 
-    switch(req.path){
+    switch (req.path) {
         case '/login':
-            require('./dynamic/login')(req,res,data,isLoggedOut)
-        return
+            require('./dynamic/login')(req, res, data, isLoggedOut)
+            return
         case '/callback':
-            require('./dynamic/callback')(req,res,db,data)
-        return
+            require('./dynamic/callback')(req, res, db, data)
+            return
         case '/proxy':
-            require('./dynamic/proxy/proxy')(req,res)
-        return
+            require('./dynamic/proxy/proxy')(req, res)
+            return
     }
     res.status(404).render('meaningOfLife.html')
 })
-app.post('/gettoken',(req,res)=>{
-    require('./dynamic/gettoken')(req,res,db,data)
+app.post('/gettoken', (req, res) => {
+    require('./dynamic/gettoken')(req, res, db, data)
 })
-app.post('/getdata',(req,res)=>{
-    require('./dynamic/getdata')(req,res,data)
+app.post('/getdata', (req, res) => {
+    require('./dynamic/getdata')(req, res, data)
 })
 
-app.listen(process.env.PORT || 80,()=>{
+app.listen(process.env.PORT || 80, () => {
     console.log('running!')
 })
 
-process.on('uncaughtException',(e)=>{
+process.on('uncaughtException', e => {
     console.log(e)
 })

94b3c0943b4a1e68f19539aab6d647665e746dc3
Ihsan Rakhman <ihsan.maulana0501@gmail.com>
2022-02-11 22:30:14 +0700

    add links

--- src/pages/navbar/navbar.tsx
+++ src/pages/navbar/navbar.tsx
@@ -1,5 +1,9 @@
 import * as React from 'react'
 
+import { NavLink } from 'react-router-dom'
+
+import './navbar.scss'
+
 interface props {
     isLoggedOut: boolean
     isDark: boolean
@@ -9,7 +13,22 @@ interface props {
 export default function Navbar({ isLoggedOut, isDark, toggleTheme }: props) {
     return (
         <nav>
-            <span>abcd</span>
+            <div>
+                <NavLink exact to="/" className="pageLink">
+                    <span>Home</span>
+                </NavLink>
+                <NavLink exact to="/top_tracks" className="pageLink">
+                    <span>Top Tracks</span>
+                </NavLink>
+                <NavLink exact to="/account" className="pageLink">
+                    <span>Account</span>
+                </NavLink>
+                <NavLink exact to="/privacy" className="pageLink">
+                    <span>Privacy Policy</span>
+                </NavLink>
+            </div>
         </nav>
     )
 }
+
+const getWindowSizeInEM = () => window.innerWidth / parseFloat(getComputedStyle(document.body).fontSize)

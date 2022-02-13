6f2c3852d2e4985d998a656332ddadf9df6c1914
Ihsan Rakhman <ihsan.maulana0501@gmail.com>
2022-02-13 11:48:49 +0700

    add profile pic

--- src/pages/navbar/navbarRight/navbarRight.tsx
+++ src/pages/navbar/navbarRight/navbarRight.tsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import './navbarRight.scss'
 
 import sunIcon from '../../../svg/light_mode.svg'
 import moonIcon from '../../../svg/dark_mode.svg'
+import defaultProfilePic from '../../../svg/profile_pic.svg'
+import dropdownIcon from '../../../svg/dropdown.svg'
+import dropdownIconLight from '../../../svg/dropdown_for_light.svg'
 
 interface props {
     isDark: boolean
@@ -11,10 +14,19 @@ interface props {
 }
 
 export default function NavbarRight({ isDark, toggleTheme }: props) {
+    const [profilePicURL, setProfilePicURL] = useState<string>(defaultProfilePic)
+    const [dropdownOpen, setDropdownOpen] = useState(false)
+
     function themeSwitcherOnClick(e?: React.KeyboardEvent<HTMLDivElement>) {
         if (e && e?.key != 'Enter') return
 
         toggleTheme()
+    }
+
+    function dropdownOnClick(e?: React.KeyboardEvent<HTMLDivElement>) {
+        if (e && e?.key != 'Enter') return
+
+        setDropdownOpen(!dropdownOpen)
     }
 
     return (
@@ -31,6 +43,27 @@ export default function NavbarRight({ isDark, toggleTheme }: props) {
                 }}
             >
                 <img src={isDark ? sunIcon : moonIcon} />
+            </div>
+            <div className="notTheThemeSwitcher">
+                <div
+                    id="dropdownHolder"
+                    onClick={() => {
+                        dropdownOnClick()
+                    }}
+                    onKeyPress={e => {
+                        dropdownOnClick(e)
+                    }}
+                >
+                    <div id="profilePic">
+                        <img src={profilePicURL} />
+                    </div>
+                    <div id="dropdown">
+                        <img
+                            src={isDark ? dropdownIcon : dropdownIconLight}
+                            style={{ transform: dropdownOpen ? 'rotate(180deg)' : '' }}
+                        />
+                    </div>
+                </div>
             </div>
         </div>
     )

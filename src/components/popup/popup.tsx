b780acecb512123b5a6b041fd5014ff9bba06d55
Ihsan Rakhman <ihsan.maulana0501@gmail.com>
2022-02-19 22:32:30 +0700

    nice closing animation

--- src/components/popup/popup.tsx
+++ src/components/popup/popup.tsx
@@ -2,6 +2,8 @@ import React, { ReactNode, useState, useEffect } from 'react'
 
 import './popup.scss'
 
+import xIcon from '../../svg/x.svg'
+
 interface props {
     children: ReactNode
     setIsOpen: React.Dispatch<React.SetStateAction<boolean>>
@@ -15,12 +17,36 @@ export default function Popup({ setIsOpen, children, title }: props) {
         setIsFullyOpened(true)
     }, [])
 
+    function closePopup() {
+        setIsFullyOpened(false)
+        setTimeout(() => {
+            setIsOpen(false)
+        }, 100)
+    }
+
     return (
         <>
-            <div className={'popupBackground ' + (isFullyOpened ? 'show ' : '')} />
+            <div
+                className={'popupBackground ' + (isFullyOpened ? 'show ' : '')}
+                onClick={() => {
+                    closePopup()
+                }}
+            />
             <div className={'popup ' + (isFullyOpened ? 'show ' : '')}>
                 <div className="topHolder">
                     <p>{title}</p>
+                    <img
+                        src={xIcon}
+                        role="button"
+                        tabIndex={1}
+                        onClick={() => {
+                            closePopup()
+                        }}
+                        onKeyPress={e => {
+                            if (e.key != 'Enter') return
+                            closePopup()
+                        }}
+                    />
                 </div>
             </div>
         </>

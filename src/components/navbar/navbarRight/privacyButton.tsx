c95754ab16b30e2593e003a5f7add6d3623e1d6c
Ihsan Rakhman <ihsan.maulana0501@gmail.com>
2022-02-26 21:33:13 +0700

    use privacyText instead

--- src/components/navbar/navbarRight/privacyButton.tsx
+++ src/components/navbar/navbarRight/privacyButton.tsx
@@ -1,12 +1,8 @@
 import React, { useState, useEffect } from 'react'
 import { useSearchParams } from 'react-router-dom'
-import ReactMarkdown from 'react-markdown'
 import Popup from '../../popup/popup'
 
-import privacyPolicyLink from '../../../files/privacyPolicyPop.md'
-import Loading from '../../loading/loading'
-
-import './privacyButton.scss'
+import PrivacyText from '../../privacyText/privacyText'
 
 interface props {
     isDark: boolean
@@ -14,24 +10,13 @@ interface props {
 
 export default function PrivacyButton({ isDark }: props) {
     const [privacyIsOpened, setPrivacyIsOpened] = useState(false)
-    const [privacyPolicyElement, setPrivacyPolicyElement] = useState<JSX.Element>(null)
     const [searchParams, setSearchParams] = useSearchParams()
 
     const popupElement = (
-        <div id="privacyPolicyPopupHolder" className={!isDark ? 'light ' : ''}>
-            <Popup title="Privacy Policy" setIsOpen={setPrivacyIsOpened} isDark={isDark}>
-                {privacyPolicyElement ?? <Loading isDark={isDark} />}
-            </Popup>
-        </div>
+        <Popup title="Privacy Policy" setIsOpen={setPrivacyIsOpened} isDark={isDark}>
+            <PrivacyText isDark={isDark} />
+        </Popup>
     )
-
-    useEffect(() => {
-        ;(async () => {
-            const data = await fetch(privacyPolicyLink)
-            const text = await data.text()
-            setPrivacyPolicyElement(<ReactMarkdown>{text}</ReactMarkdown>)
-        })()
-    }, [])
 
     useEffect(() => {
         if (searchParams.get('privacy') === null) return
---
+++ src/components/privacyText/privacyText.tsx
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from 'react'
+import ReactMarkdown from 'react-markdown'
+
+import './privacyText.scss'
+
+import Loading from '../loading/loading'
+
+import privacyPolicyURL from './privacyPolicy_a.md'
+
+interface props {
+    isDark: boolean
+}
+
+export default function PrivacyText({ isDark }: props) {
+    const [isLoading, setIsLoading] = useState(true)
+    const [textElement, setTextElement] = useState<JSX.Element>(null)
+
+    useEffect(() => {
+        getPrivacyText()
+    }, [])
+
+    async function getPrivacyText() {
+        const raw = await fetch(privacyPolicyURL)
+        const text = await raw.text()
+
+        setTextElement(<ReactMarkdown>{text}</ReactMarkdown>)
+        setIsLoading(false)
+    }
+
+    return (
+        <div id="privacyText" className={!isDark ? 'light ' : ''}>
+            {isLoading ? <Loading isDark={isDark} /> : null}
+            {textElement}
+        </div>
+    )
+}

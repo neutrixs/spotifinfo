2ec5d17c5548907cce36d2a8c46ab1de60b46c1e
Ihsan Rakhman <ihsan.maulana0501@gmail.com>
2022-02-28 18:16:41 +0700

    remove unnecessary

--- config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    scope:'user-read-email user-read-private user-read-recently-played user-top-read user-read-playback-position user-read-playback-state user-read-currently-playing',
-    client_id:'170452a430ad4323beb60b9e871dcf77',
-    client_secret:'57f4f7cc733b4950a274c422e4fa0ea0',
-    redirect_uri:'https://spotifinfo.neutrix-web.xyz/callback',
-    firestore:false,
-    pass:'KontolBesar69420'
-}

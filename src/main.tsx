d415bfb5a333a99c756d46787931095c538f4ad4
Ihsan Rakhman <ihsan.maulana0501@gmail.com>
2022-03-10 16:20:01 +0700

    load all of them at the same time

--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,15 @@ async function run() {
      * Modules that's only needed in specific parts of the site must be dynamically imported
      */
 
-    await import('react-router-dom')
-    const { default: React } = await import('react')
-    const { render } = await import('react-dom')
-    const { default: MainComponent } = await import('./pages/mainComponent')
+    const reactRouterDOMImport = import('react-router-dom')
+    const reactImport = import('react')
+    const reactDOMImport = import('react-dom')
+    const MainComponentImport = import('./pages/mainComponent')
+
+    await reactRouterDOMImport
+    const { default: React } = await reactImport
+    const { render } = await reactDOMImport
+    const { default: MainComponent } = await MainComponentImport
 
     render(<MainComponent />, document.getElementById('root'))
 }

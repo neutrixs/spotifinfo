cdbdde8cf3fec97401c95bbbd24c316fb716295d
Ihsan Rakhman <ihsan.maulana0501@gmail.com>
2022-03-10 17:48:46 +0700

    not needed

--- src/pages/mainComponent.tsx
+++ src/pages/mainComponent.tsx
@@ -1,5 +1,4 @@
 import React, { useState, lazy, Suspense, useEffect } from 'react'
-import { render } from 'react-dom'
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 
 import './base.scss'

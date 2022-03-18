99572eb8de40df11bf937f4cabc28412f2284418
Ihsan Rakhman <ihsan.maulana0501@gmail.com>
2022-03-18 20:46:06 +0700

    recaptcha badge css module

--- src/components/recaptchaBadge/recaptchaBadge.tsx
+++ src/components/recaptchaBadge/recaptchaBadge.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react'
 
-import './recaptchaBadge.scss'
+import style from './recaptchaBadge.module.scss'
+import '../hideGrecaptcha/hideGrecaptcha.scss'
 
 interface props {
     isDark: boolean
@@ -9,7 +10,7 @@ interface props {
 
 export default function RecaptchaBadge(props: props) {
     return (
-        <div id="recaptchaBadge" className={!props.isDark ? 'light' : ''} style={props.overrideStyle ?? null}>
+        <div className={style.badge + ' ' + (!props.isDark ? style.light : '')} style={props.overrideStyle ?? null}>
             This site is protected by reCAPTCHA and the Google
             <a href="https://policies.google.com/privacy"> Privacy Policy </a> and
             <a href="https://policies.google.com/terms"> Terms of Service </a> apply.

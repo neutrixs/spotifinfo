0614b5e2a0d1f6ad865bb9b75b24c13be74ef383
Ihsan Rakhman <ihsan.maulana0501@gmail.com>
2022-03-18 19:24:09 +0700

    404 page css mod

--- src/pages/404/404.tsx
+++ src/pages/404/404.tsx
@@ -1,14 +1,14 @@
 import * as React from 'react'
 
-import './404.scss'
+import style from './404.module.scss'
 
 export default function Page404() {
     return (
-        <div id="pageHolder404">
+        <div className={style.holder}>
             <div>
-                <p id="text">404</p>
-                <p id="notFound">NOT FOUND</p>
-                <p id="info">
+                <p className={style.text}>404</p>
+                <p className={style.notFound}>NOT FOUND</p>
+                <p className={style.info}>
                     THE REQUESTED RESOURCE {'\u00a0'}
                     {location.pathname.toUpperCase()}
                     {'\u00a0'} COULD NOT BE FOUND

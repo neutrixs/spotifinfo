46412591cae38ec92da1289c37e30545b1b923a9
Ihsan Rakhman <ihsan.maulana0501@gmail.com>
2022-03-19 22:24:40 +0700

    add window on click

--- src/components/navbar/navbarRight/navbarRight.tsx
+++ src/components/navbar/navbarRight/navbarRight.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 import ThemeSwitcher from './themeSwitcher/themeSwitcher'
 
@@ -9,7 +9,34 @@ interface props {
     toggleTheme: () => void
 }
 
+/**
+ * So that it passes by reference
+ */
+
+const isLocked = {
+    isLocked: false,
+}
+
 export default function NavbarRight({ isDark, toggleTheme }: props) {
+    const [dropdownIsOpen, setDropdownIsOpen] = useState(false)
+
+    useEffect(() => {
+        window.addEventListener('click', windowOnClick)
+
+        return () => {
+            window.removeEventListener('click', windowOnClick)
+        }
+    }, [])
+
+    function windowOnClick() {
+        if (isLocked.isLocked) {
+            isLocked.isLocked = false
+            return
+        }
+
+        setDropdownIsOpen(false)
+    }
+
     return (
         <div className={style.navbarRight}>
             <ThemeSwitcher isDark={isDark} toggleTheme={toggleTheme} />

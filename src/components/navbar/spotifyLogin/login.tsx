1178c17c08465f73543ceb8c1f98e3e1e4b39318
Ihsan Rakhman <ihsan.maulana0501@gmail.com>
2022-03-19 12:26:22 +0700

    spotify login comp

--- src/components/navbar/spotifyLogin/login.tsx
+++ src/components/navbar/spotifyLogin/login.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 import spotifyLogo from '../../../img/spotify_logo.png'
 import spotifyLogoLight from '../../../img/Spotify_Icon_RGB_Black.png'
 
-import './login.scss'
+import style from './style.module.scss'
 
 interface props {
     isDark: boolean
@@ -13,11 +13,11 @@ export default function Login({ isDark }: props) {
     const isForce = localStorage.getItem('force') === 'true'
 
     return (
-        <a id="spotifyLogin" href={'/login?force=' + isForce}>
-            <div className="logoHolder">
+        <a className={style.spotifyLogin} href={'/login?force=' + isForce}>
+            <div className={style.logoHolder}>
                 <img src={isDark ? spotifyLogo : spotifyLogoLight} alt="Spotify Logo" />
             </div>
-            <div className="textHolder">
+            <div className={style.textHolder}>
                 <span>Login</span>
             </div>
         </a>

9a57540c0aa3f72f87cfc9b116285f88c0dc11ac
Ihsan Rakhman <ihsan.maulana0501@gmail.com>
2022-05-27 22:03:11 +0700

    refactor account page (kinda)

--- src/pages/account/account.tsx
+++ src/pages/account/account.tsx
@@ -13,73 +13,62 @@ import style from './account.module.scss'
 
 export default function AccountPage() {
     const isMobile = useIsMobile(66.5)
-    const [isLoading, setIsLoading] = useState(true)
-
-    const [profilePicURL, setProfilePicURL] = useState('')
-    const [username, setUsername] = useState('')
-    const [email, setEmail] = useState('')
-    const [id, setId] = useState('')
-    const [followers, setFollowers] = useState(0)
-    const [plan, setPlan] = useState('')
-
-    const [isUsingDefaultPFP, setIsUsingDefaultPFP] = useState(false)
+    const [data, setData] = useState<spotifyCurrentUser | null>(null)
 
     useEffect(() => {
         getData()
     }, [])
 
-    async function getData() {
-        const rawResponse = await fetch('https://api.spotify.com/v1/me', {
+    async function getData(): Promise<void> {
+        const token = localStorage.getItem('token')
+
+        if (!token) {
+            await getToken()
+            return getData()
+        }
+
+        const response = await fetch('https://api.spotify.com/v1/me', {
             method: 'GET',
             headers: {
-                Authorization: localStorage.getItem('token') || '',
+                Authorization: token,
             },
         })
 
-        switch (rawResponse.status) {
+        switch (response.status) {
             case 400:
             case 401:
             case 403:
                 await getToken()
-                getData()
-                return
+                return getData()
         }
 
-        const data = (await rawResponse.json()) as spotifyCurrentUser
+        const parsedData = (await response.json()) as spotifyCurrentUser
 
-        setProfilePicURL(data.images[0]?.url ?? defaultProfilePic)
-
-        if (!data.images[0]?.url) {
-            setIsUsingDefaultPFP(true)
-        }
-
-        setUsername(data.display_name || '')
-        setEmail(data.email || '')
-        setId(data.id)
-        setFollowers(data.followers.total)
-        setPlan(data.product || '')
-
-        setIsLoading(false)
+        setData(parsedData)
     }
+
+    const profilePicURL = data?.images[0]?.url
+    const dataElement = (
+        <div className={style.content}>
+            <p className={style.title}>Your Account</p>
+            <img src={profilePicURL ?? defaultProfilePic} style={{ borderRadius: !profilePicURL ? '50%' : '' }} />
+            <div>
+                <p className={style.username}>{data?.display_name ?? 'unknown'}</p>
+                <p className={style.info}>
+                    Email: <span className={style.copy}>{data?.email ?? 'unknown'}</span>
+                </p>
+                <p className={style.info}>
+                    ID: <span className={style.copy}>{data?.id ?? 'unknown'}</span>
+                </p>
+                <p className={style.info}>Followers: {data?.followers.total ?? 'unknown'}</p>
+                <p className={style.info}>Plan: {data?.product ?? 'unknown'}</p>
+            </div>
+        </div>
+    )
 
     return (
         <div className={style.holder + ' ' + (isMobile ? style.mobile : '')}>
-            {isLoading ? <Loading /> : null}
-            <div className={style.content} style={{ display: isLoading ? 'none' : '' }}>
-                <p className={style.title}>Your Account</p>
-                <img src={profilePicURL} style={{ borderRadius: isUsingDefaultPFP ? '50%' : '' }} />
-                <div>
-                    <p className={style.username}>{username}</p>
-                    <p className={style.info}>
-                        Email: <span className={style.copy}>{email}</span>
-                    </p>
-                    <p className={style.info}>
-                        ID: <span className={style.copy}>{id}</span>
-                    </p>
-                    <p className={style.info}>Followers: {followers}</p>
-                    <p className={style.info}>Plan: {plan}</p>
-                </div>
-            </div>
+            {data ? dataElement : <Loading />}
             <RecaptchaBadge />
         </div>
     )

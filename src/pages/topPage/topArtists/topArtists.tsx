600db9ba4cf2e78d2bf3d540510e102e92188fe8
Ihsan Rakhman <ihsan.maulana0501@gmail.com>
2022-06-06 17:01:48 +0700

    refactor top artists (maybe)

--- src/pages/topPage/topArtists/topArtists.tsx
+++ src/pages/topPage/topArtists/topArtists.tsx
@@ -1,12 +1,9 @@
 import * as React from 'react'
 import { useState, useEffect } from 'react'
-
 import { typeSelector, rangeSelector } from '../topPage'
-
+import getToken from '../../../scripts/getToken'
 import Loading from '../../../components/loading/loading'
-
-import getTopArtists from './getTopArtists'
-
+import spotifyTopArtistsType from '../../../types/spotifyTopArtists'
 import style from '../topTrAndAr.module.scss'
 
 interface props {
@@ -16,20 +13,69 @@ interface props {
 }
 
 export default function TopArtists({ selectedType, selectedRange, targetRange }: props) {
-    const [data, setData] = useState<JSX.Element[]>([])
-    const [loading, setIsLoading] = useState(true)
+    const [data, setData] = useState<spotifyTopArtistsType | null>(null)
+    const isLoading = !data
+    const isShow = selectedType == typeSelector.artists && selectedRange == targetRange
 
     useEffect(() => {
-        getTopArtists(targetRange, setData, setIsLoading)
+        getData()
     }, [])
 
-    function isShow() {
-        return selectedType == typeSelector.artists && selectedRange == targetRange
+    async function getData(): Promise<void> {
+        const apiRangeName: { [key in rangeSelector]: string } = {
+            [rangeSelector.allTime]: 'long_term',
+            [rangeSelector.sixMonth]: 'medium_term',
+            [rangeSelector.oneMonth]: 'short_term',
+        }
+
+        const time_range = apiRangeName[targetRange]
+        const limit = '50'
+
+        const params = new window.URLSearchParams({
+            time_range,
+            limit,
+        })
+
+        const Authorization = localStorage.getItem('token')
+
+        if (!Authorization) {
+            await getToken()
+            return getData()
+        }
+
+        const response = await fetch('https://api.spotify.com/v1/me/top/artists?' + params.toString(), {
+            method: 'GET',
+            headers: {
+                Authorization,
+            },
+        })
+
+        const parsedResponse = (await response.json()) as spotifyTopArtistsType
+
+        setData(parsedResponse)
+    }
+
+    function renderData(): React.ReactNode {
+        if (!data) return null
+
+        return data.items.map<React.ReactNode>((item, i) => (
+            <div key={item.id + i} className={style.content}>
+                <div className={style.number}>
+                    <span>{i + 1}</span>
+                </div>
+                <a href={item.external_urls.spotify} className={style.artHolder}>
+                    <img src={item.images[1].url} alt={item.name} />
+                </a>
+                <a href={item.external_urls.spotify} className={style.infoHolder}>
+                    <p>{item.name}</p>
+                </a>
+            </div>
+        ))
     }
 
     return (
-        <div className={style.topArtistsHolder} style={{ display: !isShow() ? 'none' : '' }}>
-            {loading ? <Loading overrideStyle={{ maxWidth: '30em' }} /> : data}
+        <div className={style.topArtistsHolder} style={{ display: !isShow ? 'none' : '' }}>
+            {isLoading ? <Loading overrideStyle={{ maxWidth: '30em' }} /> : renderData()}
         </div>
     )
 }

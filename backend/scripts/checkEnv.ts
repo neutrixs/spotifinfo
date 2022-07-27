4d151b948cb1cecea26888511920c5a75dd7ff82
Ihsan Rakhman <ihsan.maulana0501@gmail.com>
2022-07-27 20:35:02 +0700

    goodbye server

--- backend/scripts/checkEnv.ts
+++
@@ -1,8 +0,0 @@
-export default function checkEnv(...envVars: string[]) {
-    envVars.forEach(name => {
-        // only check undefined because the env var might be set to empty string on purpose
-        if (process.env[name] === undefined) {
-            throw `MISSING ENV VAR ${name}`
-        }
-    })
-}
--- backend/types/databaseTypes.ts
+++
@@ -1,7 +0,0 @@
-interface databaseType {
-    refresh_token: string
-}
-
-type databaseTypes = { [key: string]: databaseType }
-
-export default databaseTypes
